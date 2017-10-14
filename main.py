aba94c83cebe5e3153c3f9261604415db24e1fe6
Kenny <kzhao7301@gmail.com>
2017-10-14 15:48:21 -0400

    extracts chapter URL from body of text

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import re
 
 reddit = praw.Reddit('tldrbot')
 subreddit = reddit.subreddit("noveltranslations")
-
 def parse_text(text):
-	pass
+	matchObj = re.search(r'\[Chapter [0-9]+ *\]\(.+\)', text)
+
+	if(matchObj):
+		print("------------------ REGEX FOUND ------------------")
+		print("Group", matchObj.group())
+		## (http:\/\/[a-zA-Z]+\.[a-zA-Z0-9\/\-\.]+)      ((http|https):\/\/[a-zA-Z]+\.[^\)]+)
+		link = re.search(r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', matchObj.group())
+		print("Link:", link.group())
+	else:
+		print("------------------ REGEX NOT FOUND ------------------")
 
 for submission in subreddit.hot(limit=10):
 	print("Title: ", submission.title)
