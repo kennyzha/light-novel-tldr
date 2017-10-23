5ebbe08d712a10e59b1b8eee174118060584eb8d
Kenny <kzhao7301@gmail.com>
2017-10-22 20:43:07 -0400

    parses url to summary api and prints summary of chapters in a post

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import praw
 import re
+import requests
+import json
 
 reddit = praw.Reddit('tldrbot')
 subreddit = reddit.subreddit("noveltranslations")
+API_KEY = ""
 def parse_text(text):
-	matchObj = re.search(r'\[Chapter [0-9]+ *\]\(.+\)', text)
+	chapter_regex_pattern = r'\[Chapter [0-9]+ *\]\(.+\)'
+	chapter_iters = re.finditer(chapter_regex_pattern, text)
 
-	if(matchObj):
-		print("------------------ REGEX FOUND ------------------")
-		print("Group", matchObj.group())
-		## (http:\/\/[a-zA-Z]+\.[a-zA-Z0-9\/\-\.]+)      ((http|https):\/\/[a-zA-Z]+\.[^\)]+)
-		link = re.search(r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', matchObj.group())
-		print("Link:", link.group())
-	else:
-		print("------------------ REGEX NOT FOUND ------------------")
+	for matchObj in chapter_iters:
+		url_regex_pattern = r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'
+		search = re.search(url_regex_pattern, matchObj.group())
+		print("Link:", search.group())
+##		print_summarys(API_KEY, 40, search.group())
+	return chapter_iters
 
-for submission in subreddit.hot(limit=10):
-	print("Title: ", submission.title)
-	print("Text: ", submission.selftext)
-	parse_text(submission.selftext)
 
+smmry_url = "http://api.smmry.com/"
+def print_summarys(api_key, sm_length, chapter_url):
+		payload = {"SM_API_KEY" : api_key, "SM_LENGTH" : sm_length, "SM_URL" : chapter_url}
+		r = requests.get(smmry_url, payload)
+		print(r.text)
+
+for submission in subreddit.new(limit=100):
+##	print("Title: ", submission.title)
+##	print("Text: ", submission.selftext)
+	chapter_urls = parse_text(submission.selftext)
