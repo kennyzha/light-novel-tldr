40d7b01f446827fcd7369f449934bec10bb363b1
Kenny <kzhao7301@gmail.com>
2017-10-30 17:05:20 -0400

    preparing for heroku deployment

--- ln-tldr/main.py
+++ ln-tldr/main.py
@@ -5,7 +5,7 @@ import json
 import sqlite3
 
 
-reddit = praw.Reddit('tldrbot')
+reddit = praw.Reddit("tldrbot")
 subreddit = reddit.subreddit("noveltranslations")
 API_KEY = ""
 
@@ -31,33 +31,37 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS posts
              (id text, title text, submission text)''')
 
-for submission in subreddit.hot(limit=10):
+for submission in subreddit.new(limit=10):
 	c.execute("SELECT count(*) FROM posts WHERE id = ?", (submission.id,))
 	data = c.fetchone()[0]
 	if data == 1:
-		print('Post id %s with title %s has been looked at.' % (submission.id, submission.title))
+		print("Post id %s with title %s has been looked at." % (submission.id, submission.title))
 		continue
 
-	print('Post id %s with title %s has not been summarized.' % (submission.id, submission.title))
+	print("Post id %s with title %s has not been summarized." % (submission.id, submission.title))
 	print("Title: ", submission.title)
 
 	chapter_urls = parse_text(submission.selftext)
-	submission_post = "Chapter summary: " + '\n'
+	submission_post = '[Chapter summary:](/s "'
 
+	if not chapter_urls:
+		continue
+		
 	for chapter_url in chapter_urls:
 		url = get_url(chapter_url)
 		print("Link:", url.group())
 		summary = summarize(API_KEY, 10, url.group()).json()
 
 		if summary.get("smi_api_error") is None:
-			submission_post += (summary.get("sm_api_content") + '\n')
+			submission_post += (summary.get("sm_api_content") + " ")
 		else:
 			print("There was an error summarizing link ", url.group(), "from post", submission.title)
-
+	submission_post += '")'
 	print("Submission_post is ", submission_post)
+
 	post = (submission.id, submission.title, submission_post,)
 	c.execute("INSERT INTO posts VALUES (?,?,?)", post)
 	conn.commit()
+	submission.reply(submission_post)
 
 conn.close()
-
